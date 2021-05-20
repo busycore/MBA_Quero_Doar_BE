6ccf8e59d46dabd92c4e4c616fa26c0162b16a2b
Tiago Gemelgo <tiago.gemelgo@outlook.com>
2021-05-19 21:38:55 -0300

    Troca de Nome para Descrição para fazer mais sentido no retorno para o usuário

--- source/ViewModel/Doacao/MinhasDoacoesCupomVM.cs
+++ source/ViewModel/Doacao/MinhasDoacoesCupomVM.cs
@@ -7,7 +7,7 @@ namespace source.ViewModel.Doacao
     public class MinhasDoacoesCupomVM
     {
         public string Id { get; set; }
-        public string Nome { get; set; }
+        public string Descricao { get; set; }
         public decimal Valor { get; set; }
     }
 }
