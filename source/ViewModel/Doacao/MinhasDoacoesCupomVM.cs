48b1e4752e0f6bdbad80a3b1f07f4ee69b6e1f4c
Matheus Vargem <matheusvrgm@gmail.com>
2021-05-24 21:33:06 -0300

    fixed cupom with frontend

--- source/ViewModel/Doacao/MinhasDoacoesCupomVM.cs
+++ source/ViewModel/Doacao/MinhasDoacoesCupomVM.cs
@@ -1,9 +1,13 @@
-﻿namespace source.ViewModel.Doacao
+﻿using source.ViewModel.Empresa;
+
+namespace source.ViewModel.Doacao
 {
     public class MinhasDoacoesCupomVM
     {
         public string Id { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
+        public string nome { get; set; }
+        public DadosEmpresaVM EmpresaParceria { get; set; }
     }
 }
