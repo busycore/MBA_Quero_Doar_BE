a15a410519e539381cf3ae92430cc06d00a00df7
Tiago Gemelgo <tiago.gemelgo@outlook.com>
2021-07-25 20:30:46 -0300

    Build

--- source/ViewModel/Instituicao/DadosInstituicaoVM.cs
+++ source/ViewModel/Instituicao/DadosInstituicaoVM.cs
@@ -13,5 +13,6 @@ namespace source.ViewModel.Instituicao
         public string Telefone { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Foto { get; set; }
     }
 }
