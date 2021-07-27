6c6da1a6e71ca1b813d2394a455719867a44a80d
Tiago Gemelgo <tiago.gemelgo@outlook.com>
2021-07-26 21:00:07 -0300

    Inclusão do campo de descrição

--- source/ViewModel/Instituicao/DadosInstituicaoVM.cs
+++ source/ViewModel/Instituicao/DadosInstituicaoVM.cs
@@ -14,5 +14,6 @@ namespace source.ViewModel.Instituicao
         public string Email { get; set; }
         public string Password { get; set; }
         public string Foto { get; set; }
+        public string Descricao { get; set; }
     }
 }
